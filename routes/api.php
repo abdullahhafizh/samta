8cdcd6b82eaac41f1a4aa028ba575aded8c31421
Abdullah Hafizh <abdllhhafizh@gmail.com>
2019-08-03 19:10:23 +0700

    query builder

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('answer', 'HomeController@answer')->name('answer');
+Route::get('answer', 'HomeController@answer')->name('answer');
